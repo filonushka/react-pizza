7c33254970d924dd3850bcd4ff94bf9b3e3e08ce
filonushka <lero4ka.filonova@mail.ru>
2022-08-04 16:55:25 +0300

    feat: added db, pizzablock

--- src/App.js
+++ src/App.js
@@ -3,14 +3,24 @@ import React from "react";
 import { Header } from "./components";
 import { Home, Cart } from "./pages";
 import { Routes, Route } from "react-router-dom";
+import axios from "axios";
 
 function App() {
+  const [pizzas, setPizzas] = React.useState([]);
+
+  React.useEffect(() => {
+    axios.get("http://localhost:3000/db.json").then(({ data }) => {
+      setPizzas(data.pizzas);
+    });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route path="/" element={<Home />} exact />
+          {/* <Route path="/" render={() => <Home items={pizzas} />} exact /> */}
+          <Route path="/" element={<Home items={pizzas} />} exact />
           <Route path="/cart" element={<Cart />} exact />
         </Routes>
       </div>
